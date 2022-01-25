eeccf36ff9a661f1045dfd66d65c3d01ab2b3d0c
Nujee <310895@mail.ru>
2022-01-25 15:58:39 +0400

    Add kinematic rigidbody on player

---
+++ Runner3D/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("triggered!");
+        }
+    }
+
+    void Update()
+    {
+        var v = Input.GetAxis("Vertical");
+        var h = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(h, 0, v) * Time.deltaTime * 10;
+
+    }
+}
